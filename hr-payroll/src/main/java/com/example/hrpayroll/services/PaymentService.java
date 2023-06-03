aec8aa5e1500864f43fc37cc013d880e16249181
rodrigowe1988 <rodrigoweber@pop-os.localdomain>
2023-06-03 20:39:57 -0300

    refactor hr-payroll

--- hr-payroll/src/main/java/com/example/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/example/hrpayroll/services/PaymentService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 public class PaymentService {
 
     public Payment getPayment(long workerId, int days) {
+
         return new Payment("Bob", 200.0, days);
     }
 }
