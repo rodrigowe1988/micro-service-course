755c26d792eefd3efc80bfca8fd2e887da2f754c
rodrigowe1988 <rodrigoweber@pop-os.localdomain>
2023-06-03 20:06:37 -0300

    implements lombok to hr-worker

--- hr-worker/src/main/java/com/devsuperior/hr/worker/entities/Worker.java
+++ hr-worker/src/main/java/com/devsuperior/hr/worker/entities/Worker.java
@@ -1,10 +1,17 @@
 package com.devsuperior.hr.worker.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "tb_worker")
 public class Worker implements Serializable {
     private static final Long serialVersionUID = 1L;
@@ -14,39 +21,6 @@ public class Worker implements Serializable {
     private Long id;
     private String name;
     private Double dailyIncome;
-
-    public Worker() {
-    }
-
-    public Worker(Long id, String name, Double dailyIncome) {
-        this.id = id;
-        this.name = name;
-        this.dailyIncome = dailyIncome;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getDailyIncome() {
-        return dailyIncome;
-    }
-
-    public void setDailyIncome(Double dailyIncome) {
-        this.dailyIncome = dailyIncome;
-    }
 
     @Override
     public boolean equals(Object o) {
