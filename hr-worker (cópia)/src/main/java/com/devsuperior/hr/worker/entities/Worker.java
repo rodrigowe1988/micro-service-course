2b390ff7f4dac329cdb6c31ebe50f4b5f3777d8e
Weber <rodrigoweber97@hotmail.com>
2023-12-17 16:06:51 -0300

    Hystrix tolerancia a falhas

---
+++ hr-worker (cópia)/src/main/java/com/devsuperior/hr/worker/entities/Worker.java
@@ -0,0 +1,24 @@
+package com.devsuperior.hr.worker.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_worker")
+public class Worker implements Serializable {
+    private static final Long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+}
